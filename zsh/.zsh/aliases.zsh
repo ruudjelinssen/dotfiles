659efd2ffa65096059c9908b715d5fb5505aa20c
Ruud Linssen <r.p.j.linssen@gmail.com>
2019-01-15 19:01:26 +0100

    Zsh files

---
+++ zsh/.zsh/aliases.zsh
@@ -0,0 +1,29 @@
+# =====================================================================
+# 
+# Aliases
+#
+# =====================================================================
+
+alias cl='clear'
+alias l='ls -la'
+alias q='exit'
+function open() {
+    xdg-open "$*" &
+}
+
+# Git
+alias ga='git add -A'
+alias gp='git push'
+alias gl='git log'
+alias gs='git status'
+alias gd='git diff'
+alias gm='git commit -m'
+alias gma='git commit -am'
+alias gb='git branch'
+alias gc='git checkout'
+alias gra='git remote add'
+alias grr='git remote rm'
+alias gpu='git pull'
+alias gcl='git clone'
+alias gta='git tag -a -m'
+alias gf='git reflog'
