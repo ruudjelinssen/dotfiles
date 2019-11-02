659e9e9168b6960be190836a8b7bd6a576a10940
Ruud Linssen <r.p.j.linssen@gmail.com>
2019-11-02 20:17:19 +0100

    What is my ip alias

--- zsh/.zsh/aliases.zsh
+++ zsh/.zsh/aliases.zsh
@@ -28,20 +28,5 @@ alias gcl='git clone'
 alias gta='git tag -a -m'
 alias gf='git reflog'
 
-# Start IRC on pi
-function irc() {
-    if [[ -z $(ifconfig | grep tun0) ]]; then
-        echo "OpenVPN not connected. Trying to connect...";
-        CONN=$(nmcli connection | grep vpn | cut -d " " -f1)
-        if [[ -z $CONN ]]; then
-            echo "No VPN connection available. Connect manually.";
-            return;
-        fi
-        nmcli connection up $CONN
-        ssh weechat@pi.weechat
-        nmcli connection down $CONN
-    else
-        echo "OpenVPN connected. Connecting to SSH.";
-        ssh weechat@pi.weechat
-    fi
-}
+# What is my ip
+alias whatsmyip='curl https://ipinfo.io/ip'
