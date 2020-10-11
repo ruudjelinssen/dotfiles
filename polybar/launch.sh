5e890b687f17d95426ca673b12d0d8afc24b9ef3
Ruud Linssen <r.p.j.linssen@student.tue.nl>
2020-10-11 13:22:32 +0200

    Better rofi

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -6,9 +6,12 @@ pkill polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch Polybar, using default config location ~/.config/polybar/config
-host=$(hostname -s)
-if [[ $host -eq "bakkie" ]]; then
-    WLAN_IF=wlp6s0 MONITOR=eDP1 polybar main-laptop &
-fi
-
+case $(hostname -s) in
+    bakkie)
+        WLAN_IF=wlp6s0 MONITOR=eDP1 polybar main-laptop &
+        ;;
+    fedora-laptop)
+        WLAN_IF=wlp2s0 MONITOR=eDP1 polybar main-laptop &
+        ;;
+esac
 echo "Polybar launched..."
