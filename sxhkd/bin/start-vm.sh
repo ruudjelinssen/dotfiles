3c266c3652607602879c65bd113206de5da93100
Ruud Linssen <r.p.j.linssen@student.tue.nl>
2020-12-08 10:48:40 +0100

    Alacritty and coc

--- sxhkd/bin/start-vm.sh
+++ sxhkd/bin/start-vm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 LIBVIRT_URI="qemu:///system"
-vm=$(virsh -c $LIBVIRT_URI list --all --name | rofi -dmenu -theme gruvbox-dark -p " ")
+vm=$(virsh -c $LIBVIRT_URI list --all --name | rofi -dmenu -theme ~/.config/rofi/themes/gruvbox-dark.rasi -p " ")
 
 if [ -z $vm ]; then
     exit
